a6c95a558354073b0d38d5edd9a107df67d85bcc
ej8899 <ej8899@gmail.com>
2022-08-08 11:21:32 -0600

    change to change into all in one object

--- dateobject.js
+++ dateobject.js
@@ -1,12 +1,27 @@
-const IntTwoChars = (i) => { // helper for dateObject
-    return (`0${i}`).slice(-2);
-  };
-const dateObject = new Date();
-    let date = IntTwoChars(dateObject.getDate());
-    let month = IntTwoChars(dateObject.getMonth() + 1);
-    let year = dateObject.getFullYear();
-    let hours = IntTwoChars(dateObject.getHours());
-    let minutes = IntTwoChars(dateObject.getMinutes());
-    let seconds = IntTwoChars(dateObject.getSeconds());
+//
+// myDateObject - date object to provide quick access to formatted date and times
+//
 
-    // TODO - convert into a full object/class to use .this and embed the IntTwoChars()
+const myDateObject = {
+  dateObject: new Date(),
+  datePad(i) {
+    return (`0${i}`).slice(-2);
+  },
+  
+  date() { return (this.datePad(this.dateObject.getDate())); },
+  month() { return (this.datePad(this.dateObject.getMonth() + 1)); },
+  year() { return (this.dateObject.getFullYear()); },
+  hours() { return (this.datePad(this.dateObject.getHours())); },
+  minutes() { return (this.datePad(this.dateObject.getMinutes())); },
+  seconds() { return (this.datePad(this.dateObject.getSeconds())); },
+  justDate() { return (`${this.year()}/${this.month()}/${this.date()}`); },
+  justTime() { return (`${this.hours()}:${this.minutes()}:${this.seconds()}`); },
+};
+
+// EXAMPLES:
+console.log(myDateObject.dateObject); // shows ZULU time
+console.log(myDateObject.date()); // show just the day
+console.log(myDateObject.month()); // show just the month
+console.log(myDateObject.year()); // show current year
+console.log(myDateObject.justDate()); // shows just the date with '-' divider between YYYY-MM-DD
+console.log(myDateObject.justTime()); // shows just the time with ':' dividier between HH:MM:SS
